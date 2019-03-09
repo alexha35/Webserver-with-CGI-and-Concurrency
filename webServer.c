163613677f147a398635be0c803becbefa93fa00
Alex Ha <AlexHa@Alexs-MacBook-Pro.local>
2019-03-09 14:23:33 -0600

    Initialized Client Connection

--- webServer.c
+++ webServer.c
@@ -14,32 +14,38 @@
 #include <fcntl.h>
 
 
-int main(int argc, char *argv[]){
-
-  //Opening HTML file
-  FILE *html_file;
-  html_file = fopen("index.html","r");
-
-  char response_buffer[1024];
-  fgets(response_buffer,1024,html_file);
+int main(){
 
   //http header
-  char headerHTTP[2048] = "HTTP/1.0 200 ok\r\n\n";
-  strcat(headerHTTP,response_buffer);
+  char headerHTTP[10000] =
+    "HTTP/1.1 200 ok\r\n"
+    "Content_Type: text/html; charset=UTF8\r\n\r\n";
+
+  //Open HTML file
+  //TODO
+
+  struct sockaddr_in serverAddress, clientAddress;
+  socklen_t sin_len = sizeof(clientAddress);
+  int serverSocket, clientSocket;
+  char buf[20000];
+  int img;
+  int active = 1;
 
   //Create socket
 
-  int serverSocket;
   serverSocket = socket(AF_INET,SOCK_STREAM,0);
   if(serverSocket < 0){
     perror("Failed: ");
     exit(1);
   }
 
-  struct sockaddr_in serverAddress;
+  setsockopt(serverSocket,SOL_SOCKET, SO_REUSEADDR, &active, sizeof(active));
+
   serverAddress.sin_family = AF_INET;
   serverAddress.sin_addr.s_addr = INADDR_ANY;
-  serverAddress.sin_port = htons(8001);
+  serverAddress.sin_port = htons(8080);
+
+
 
 
   //bind to socket
@@ -48,21 +54,35 @@ int main(int argc, char *argv[]){
   bindSocket = bind(serverSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
   if(bindSocket < 0){
     perror("Failed: ");
+    close(serverSocket);
     exit(1);
   }
 
   //listen to socket
 
-  listen(serverSocket,5);
+  int servListen;
+  servListen = listen(serverSocket,5);
+  if(servListen < 0){
+    perror("Failed: ");
+    close(serverSocket);
+    exit(1);
+  }
 
   //accept socket
 
-  int clientSocket;
-
   while(1){
-    clientSocket = accept(serverSocket,NULL,NULL);
-    send(clientSocket,headerHTTP,sizeof(headerHTTP),0);
+    clientSocket = accept(serverSocket,(struct sockaddr *) &clientAddress,&sin_len);
+    printf("Got client connection \n");
+
+    if(!fork()){
+      close(serverSocket);
+      memset(buf,0,20000);
+      read(clientSocket,buf,2047);
+
+      write(clientSocket,headerHTTP, sizeof(headerHTTP) - 1);
+      close(clientSocket);
+    }
+    close(clientSocket);
     //close(clientSocket);
   }
-  return 0;
 }
