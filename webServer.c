866f2023f3da907c0ac4212d1b51b1109bfcfbd3
Alex Ha <AlexHa@Alexs-MacBook-Pro.local>
2019-03-16 03:12:27 -0500

    Updated socket etc

--- webServer.c
+++ webServer.c
@@ -124,22 +124,59 @@ int main(int argc, char *argv[]){
   fclose(file);
 
   //All necessary variables (sockets, sockaddr_in, buffer)
-
+  int serverSocket;
+  int clientSocket;
+  int bindsuccess;
+  int listenSuccess;
+  struct sockaddr_in serverAddress;
+  struct sockaddr_in clientAddress;
+  socklen_t sinLen = sizeof(clientAddress);
+  char buffer[10000];
+  int fdimg;
+  int status = 1;
 
   //socket
-
+  serverSocket = socket(AF_INET,SOCK_STREAM, 0);
+  if(serverSocket < 0){
+    perror("Socket:");
+    exit(1);
+  }
 
   //initiate sockaddr_in
-
+  setsockopt(serverSocket,SOL_SOCKET,SO_REUSEADDR, &status, sizeof(int));
+  serverAddress.sin_family = AF_INET;
+  serverAddress.sin_addr.s_addr = INADDR_ANY;
+  serverAddress.sin_port = htons(8080);
 
   //bindSocket
-
+  bindsuccess = bind(serverSocket, (struct sockaddr *) &serverAddress, sizeof(serverAddress));
+  if(bindsuccess < 0){
+    perror("Bind: ");
+    exit(1);
+  }
 
   //Listen
-
+  listenSuccess = listen(serverSocket, 10);
+  if(listenSuccess < 0){
+    perror("Listen: ");
+    exit(1);
+  }
 
   //Accept
+  while(1){
+    clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddress, &sinLen);
+    if(clientSocket < 0){
+      perror("Accept: ");
+      continue;
+    }
+    //debug
+    printf("%s\n","Connected");
 
+    //child process
+    if(!fork()){
+
+    }
+  }
 
   return 0;
 }
