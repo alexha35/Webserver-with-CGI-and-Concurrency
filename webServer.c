020b3be47168479f71ad027a4f50ac6a020655e5
Alex Ha <AlexHa@Alexs-MacBook-Pro.local>
2019-03-17 19:41:58 -0500

    update

--- webServer.c
+++ webServer.c
@@ -209,7 +209,7 @@ strcat(webpage, str);
 
       if(strncmp(buffer, "GET /images/", 12) == 0){
         write(clientSocket,imgheader, sizeof(imgheader) - 1);
-        fdimg = open("/images/ApexLegends.jpeg", O_RDONLY);
+        fdimg = open("/images/ApexLegends.jpg", O_RDONLY);
         sendfile(clientSocket,fdimg, NULL, 100000);
         close(fdimg);
       }
